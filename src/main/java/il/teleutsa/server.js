8b7247661f523980c4ce11ee8af7907ecc0cd060
Zury2005 <teleutsabuis@gmail.com>
2022-07-26 13:55:16 +0300

    Initial

---
+++ src/main/java/il/teleutsa/server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer(function (request, response) {
+    console.log(request.method, request.url)
+    if (request.url == '/style.css') {
+        const css = fs.readFileSync('style.css', "utf-8");
+        response.end(css);
+    } else {
+        const text = fs.readFileSync('index.html', "utf-8");
+        response.end(text);
+    }
+});
+
+server.listen(3000);
+console.log("server started");
